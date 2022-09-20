34fba3e7cc15061a6da8089f45707527719b5d9e
Ismail <ismail28051996@gmail.com>
2022-09-21 02:08:57 +0530

    Design_Work

--- src/component/basics/MenuCard.js
+++ src/component/basics/MenuCard.js
@@ -11,7 +11,7 @@ const MenuCard = ({ menues }) => {
               <div className="card">
                 <div className="card-body">
                   <span className="card-number card-circle subtle"> {curElement.id} </span>
-                  <span className="card-author subtle">{curElement.name}</span>
+                  {/* <span className="card-author subtle">{curElement.name}</span> */}
                   <h3 className="card-title">{curElement.name}</h3>
                   <span className="card-description subtle">
                   {curElement.description}
@@ -19,7 +19,7 @@ const MenuCard = ({ menues }) => {
                   <div className="card-read">Read</div>
                 </div>
                 <img src={curElement.image} alt="images" className="card-media" />
-                <span className="card-tag subtle">Order Now</span>
+                <button className="card-tag subtle btn-dangers">Order Now</button>
               </div>
             </div>
           </>
