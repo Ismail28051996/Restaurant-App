262d62e9183a49db3fc609f677d92ac47b702fc8
Ismail <ismail28051996@gmail.com>
2022-09-22 02:11:21 +0530

    Card Design work done

--- src/component/basics/MenuCard.js
+++ src/component/basics/MenuCard.js
@@ -10,16 +10,18 @@ const MenuCard = ({ menues }) => {
             <div className="card-container" key={curElement.id}>
               <div className="card">
                 <div className="card-body">
-                  <span className="card-number card-circle subtle"> {curElement.id} </span>
+                  <center><span className="card-number card-circle subtle"> {curElement.id} </span></center><br />
                   {/* <span className="card-author subtle">{curElement.name}</span> */}
-                  <h3 className="card-title">{curElement.name}</h3>
+                  {/* <h3 className="card-title">{curElement.name}</h3> */}
+                  <img src={curElement.image} alt="images" className="card-media" />
+                  <center><h3 className="card-title">{curElement.name}</h3></center>
+                  <div className="card-read">Read</div>
                   <span className="card-description subtle">
                   {curElement.description}
-                  </span>
-                  <div className="card-read">Read</div>
+                  </span><br /><br />
+                  <center><button className="card-tag subtle">Order Now</button></center>
                 </div>
-                <img src={curElement.image} alt="images" className="card-media" />
-                <button className="card-tag subtle btn-dangers">Order Now</button>
+                
               </div>
             </div>
           </>
